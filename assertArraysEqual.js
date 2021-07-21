308242acdf0ce0a24ae3d986b7f97e59fcacca35
Jonathan Choi <jonchoi11@gmail.com>
2021-07-21 04:24:46 +0000

    adding assert arrays equalizer function

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: Everything looks good!`);
+  } else {
+    console.log(`❌ Assertion Failed: Everything looks bad!`)
+  }
+};
