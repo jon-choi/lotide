ec77f4b41675f325d4cf5f9308bf26e301483ef5
Jonathan Choi <jonchoi11@gmail.com>
2021-07-21 23:35:07 +0000

    counting letters in objects

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let letterCount = {};
+
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters('Is this going to work?'));
