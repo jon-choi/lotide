3ecbfae8f531f433815cf84aad271365f620773f
Jonathan Choi <jonchoi11@gmail.com>
2021-07-22 01:41:22 +0000

    finding keys by their values

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+const favouriteCuisinesByDish = {
+  japanese: 'ramen',
+  italian: 'fettucine alfredo',
+  indian: 'butter chicken'
+};
+
+assertEqual(findKeyByValue(favouriteCuisinesByDish, 'ramen'), 'japanese');
+assertEqual(findKeyByValue(favouriteCuisinesByDish, 'butter chicken'), 'indian');
+assertEqual(findKeyByValue(favouriteCuisinesByDish, 'poutine'), 'undefined');
