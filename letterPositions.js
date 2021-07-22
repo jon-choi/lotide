23eda96c4232d4618a0ea90fa5a0bb0ccb1f6752
Jonathan Choi <jonchoi11@gmail.com>
2021-07-22 00:24:53 +0000

    add letter positions

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: Everything looks good!`);
+  } else {
+    console.log(`❌ Assertion Failed: Everything looks bad!`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  let position = 0;
+
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(position);
+      } else {
+        results[letter] = [];
+        results[letter].push(position);
+      }
+    }
+    position++;
+  }
+  return results;
+};
+console.log(letterPositions('lighthouse in the house'));
+assertArraysEqual(letterPositions('hello').h, [0]);
+assertArraysEqual(letterPositions('hello').l, [2, 3]);
+assertArraysEqual(letterPositions('hello').o, [4]);
+assertArraysEqual(letterPositions('hello').e, [1]);
