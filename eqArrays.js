ba1efe28c47cced0a028b9101c691aa7d0fb7562
Jonathan Choi <jonchoi11@gmail.com>
2021-07-26 19:44:49 +0000

    refactored to modularize testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(actual, expected) {
   // returns false if array lengths are not the same
@@ -20,7 +14,5 @@ const eqArrays = function(actual, expected) {
   // returns true if conditions above do not run
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+
+module.exports = eqArrays;
